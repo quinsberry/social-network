d466a59ba172a0358dd38fc3763f288b5765167d
quinsberry <fatbobisreal@gmail.com>
2020-04-02 12:01:46 +0300

    Updates

---
+++ precache-manifest.8f156bb2cf3432f3091ea0d397e8f713.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e52bacf06e2d1dc0beb9db3f381c13",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "a77381ca92242e53f2ba",
+    "url": "/social-network/static/css/3.5283b10c.chunk.css"
+  },
+  {
+    "revision": "38ce15693a429e28bcbf",
+    "url": "/social-network/static/css/4.a14e08fa.chunk.css"
+  },
+  {
+    "revision": "97ee04bdf212e0606e3e",
+    "url": "/social-network/static/css/main.5e4e1e5a.chunk.css"
+  },
+  {
+    "revision": "fa312081e8a99faa2aba",
+    "url": "/social-network/static/js/2.4a11a05e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/social-network/static/js/2.4a11a05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77381ca92242e53f2ba",
+    "url": "/social-network/static/js/3.8dbbe413.chunk.js"
+  },
+  {
+    "revision": "38ce15693a429e28bcbf",
+    "url": "/social-network/static/js/4.b417fbfa.chunk.js"
+  },
+  {
+    "revision": "97ee04bdf212e0606e3e",
+    "url": "/social-network/static/js/main.a3f83ba1.chunk.js"
+  },
+  {
+    "revision": "feda7033d4f475f8d5e6",
+    "url": "/social-network/static/js/runtime-main.d3217f89.js"
+  },
+  {
+    "revision": "01c9ef5c9ed3fbdf9bdde57df2b4db7f",
+    "url": "/social-network/static/media/caret-left.01c9ef5c.svg"
+  },
+  {
+    "revision": "3ea9033931be59445698a31805f5bd8c",
+    "url": "/social-network/static/media/caret-right.3ea90339.svg"
+  },
+  {
+    "revision": "2bd8db19732c901d51802c2e0e67a523",
+    "url": "/social-network/static/media/face.2bd8db19.jpg"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/social-network/static/media/facebook.34e73505.svg"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/social-network/static/media/github.ec5bc1b3.svg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/social-network/static/media/instagram.e75317fa.svg"
+  },
+  {
+    "revision": "6743c3ba2e5eb5308cb0774158c2be9f",
+    "url": "/social-network/static/media/logo.6743c3ba.svg"
+  },
+  {
+    "revision": "834b5767260b78f7aae217d985fa34d1",
+    "url": "/social-network/static/media/logout.834b5767.svg"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/social-network/static/media/twitter.6620c8ae.svg"
+  },
+  {
+    "revision": "577fbad0ff6ee6e28ee200f2d7b4d1c3",
+    "url": "/social-network/static/media/userphoto_default.577fbad0.png"
+  },
+  {
+    "revision": "60e31664870895cd8d5af4fd2137f153",
+    "url": "/social-network/static/media/vk.60e31664.svg"
+  },
+  {
+    "revision": "9901c4dd3d46a5cd4bc5e3d1c76fa0c8",
+    "url": "/social-network/static/media/website.9901c4dd.svg"
+  },
+  {
+    "revision": "c80ca203c0f773a8502d85ae60f347ce",
+    "url": "/social-network/static/media/youtube.c80ca203.svg"
+  }
+]);
